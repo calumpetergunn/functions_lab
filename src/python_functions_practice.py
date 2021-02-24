7c7a0d2f395d0b47a64bb02e5831b7568bb1d023
calumpetergunn <calumgunn@gmail.com>
2021-02-24 14:09:33 +0000

    added lenght of string function

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -9,3 +9,9 @@ def subtract(number_1, number_2):
 
 def multiply(number_1, number_2):
     return number_1 * number_2
+
+def divide(number_1, number_2):
+    return number_1 / number_2
+
+def length_of_string(string):
+    return len(string)
